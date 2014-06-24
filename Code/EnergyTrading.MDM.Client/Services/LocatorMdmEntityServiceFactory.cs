937152f3ade6b717a29f2a3ec28cabf008ad3343
Simon Hanlon <simon.hanlon@rwe.com>
2014-06-24 16:09:21 +0100

    updated to include the version as well as the type in the cache key

--- Code/EnergyTrading.MDM.Client/Services/LocatorMdmEntityServiceFactory.cs
+++ Code/EnergyTrading.MDM.Client/Services/LocatorMdmEntityServiceFactory.cs
@@ -8,22 +8,27 @@
     public class LocatorMdmEntityServiceFactory : IMdmEntityServiceFactory
     {
         private readonly IServiceLocator locator;
-        private readonly ConcurrentDictionary<Type, object> cache;
+        private readonly ConcurrentDictionary<string, object> cache;
 
         public LocatorMdmEntityServiceFactory(IServiceLocator locator)
         {
             this.locator = locator;
-            this.cache = new ConcurrentDictionary<Type, object>();
+            this.cache = new ConcurrentDictionary<string, object>();
+        }
+
+        private string CreateCacheKey(Type type, uint version)
+        {
+            return version.ToString() + type.ToString();
         }
 
         public IMdmEntityService<TContract> EntityService<TContract>(uint version = 0) where TContract : EnergyTrading.Mdm.Contracts.IMdmEntity
         {
-            var type = typeof(TContract);
+            var key = this.CreateCacheKey(typeof(TContract), version);
             object value;
-            if (!this.cache.TryGetValue(type, out value))
+            if (!this.cache.TryGetValue(key, out value))
             {
                 value = version == 0 ? this.locator.GetInstance<IMdmEntityService<TContract>>() : this.locator.GetInstance<IMdmEntityService<TContract>>("V" + version);
-                this.cache.AddOrUpdate(type, value, (x, y) => value);
+                this.cache.AddOrUpdate(key, value, (x, y) => value);
             }
 
             return (IMdmEntityService<TContract>)value;
