be481d782f2c878d753819347cd0310b7c869ccb
RWEST TeamCity <RWESTTeamCity@rwe.com>
2014-10-22 09:11:55 +0100

    Package updates from CI build

--- Code/EnergyTrading.MDM.Client.Tests/Properties/AssemblyInfo.cs
+++ Code/EnergyTrading.MDM.Client.Tests/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("115")]
-[assembly: AssemblyFileVersion("115")]
+[assembly: AssemblyVersion("116")]
+[assembly: AssemblyFileVersion("116")]
